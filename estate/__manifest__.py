fa41df3e89f81350783e466172b3bb7103ee6470
Tzvi <hersheyb@measuredleap.com>
2025-04-22 21:00:25 +0400

    initial security ch4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,9 +5,13 @@
     'description': 'This module is a basic real estate listing system.',
     'category': 'Real Estate',
     'author': 'measured-leap',
-    'website': 'https://github.com/measured-leap/tutorials',
+    'website': 'https://github.com/measured-leap/tutorials/tree/estate-module/estate',
     'license': 'LGPL-3',
     'depends': ['base'],
     'installable': True,
-    'application': True
+    'application': True,
+    'data': [
+        'security/estate_security.xml',
+        'security/ir.model.access.csv'
+    ]
 }
